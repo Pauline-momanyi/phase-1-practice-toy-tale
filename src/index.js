34926327afb9048367f44704ca878508d0d2a2a8
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-18 23:32:24 +0300

    Complete

--- src/index.js
+++ src/index.js
@@ -15,28 +15,35 @@ document.addEventListener("DOMContentLoaded", () => {
   
   //Manipulate DOM to display toys  
   function renderToy(toy){   
-      let toyDiv = document.getElementById('toy-collection')
+      //let toyDiv = document.getElementById('toy-collection')
       let card = document.createElement('div')
       card.className = 'card'
-      console.log(toyDiv);
+      //console.log(toyDiv);
       card.innerHTML = `    
         <h2>${toy.name}</h2>
         <img src="${toy.image}" class="toy-avatar" />
         <p><span id='likes'>${toy.likes}</span> Likes </p>
-        <button class="like-btn" id="${toy.id}">Like</button>
-      ` 
-      toyDiv.appendChild(card)  
+        <button class="like-btn" id="like">Like</button>
+      `  
+     
       //edit likes using PATCH
-      let likebutton = document.querySelector('.like-btn')
-//console.log(likebutton);
-likebutton.addEventListener('click', ()=>{
+      // let likebutton = document.querySelector('.like-btn')
+      // console.log(likebutton);
+      card.querySelector('#like').addEventListener('click', (e)=>{
   //console.log(toy.likes);
-  toy.likes += 1
-  document.querySelector('span').textContent = toy.likes
-  updateLikes(toy)
-})
+      e.preventDefault
+      toy.likes += 1
+      card.querySelector('span').textContent = toy.likes
+   updateLikes(toy) 
+  
+
+    })
+    document.getElementById('toy-collection').appendChild(card)
+
+   
   }
 
+ 
 
   //fetch toys
   function getToys(){
@@ -46,53 +53,53 @@ likebutton.addEventListener('click', ()=>{
       console.log(toyData);
       toyData.forEach(toy => renderToy(toy))})
   }
-  // function initializetoys(){
-  //   getToys()
-  // }
-  // initializetoys()
-  getToys()
+  function initializetoys(){
+    getToys()
+  }
+  initializetoys()
+  //getToys()
 
 
   //POST toy
 
-document.querySelector('.add-toy-form').addEventListener('submit',handleSubmit)
+  document.querySelector('.add-toy-form').addEventListener('submit',handleSubmit)
 
-function handleSubmit(e){
-  e.preventDefault()
-  let toyObj = {
-    name: e.target.querySelector('#toyName').value,
-    image: e.target.querySelector('#toyImage').value,
-    likes: 0
+  function handleSubmit(e){
+    e.preventDefault()
+    let toyObj = {
+      name: e.target.querySelector('#toyName').value,
+      image: e.target.querySelector('#toyImage').value,
+      likes: 0
 
+    }
+    renderToy(toyObj)
+    postToys(toyObj)
   }
-  renderToy(toyObj)
-  postToys(toyObj)
-}
 
-function postToys(toyObj){
-  fetch('http://localhost:3000/toys',{
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(toyObj)
-  })
-  .then(res => res.json())
-  .then(toyData=>console.log(toyData))
-}
+  function postToys(toyObj){
+    fetch('http://localhost:3000/toys',{
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(toyObj)
+    })
+    .then(res => res.json())
+    .then(toyData=>console.log(toyData))
+  }
 
 
-//UPDATE
-function updateLikes(toyObj){
-  fetch(`http://localhost:3000/toys/${toyObj.id}`,{
-    method: "PATCH",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(toyObj)
-  })
-  .then(res => res.json())
-  .then(toyData=>console.log(toyData))
-}
+  //UPDATE
+  function updateLikes(toyObj){
+    fetch(`http://localhost:3000/toys/${toyObj.id}`,{
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(toyObj)
+    })
+    .then(res => res.json())
+    .then(toyData=>console.log(toyData))
+  }
 
 });
